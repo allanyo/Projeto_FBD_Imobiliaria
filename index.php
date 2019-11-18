344b33e0499e44746b38c68250efd22586b54e1b
Allanyo Santos <allanyosantos@gmail.com>
2019-11-17 22:55:16 -0300

    Nova pagina Inicial com Carrossel

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="css/estilo.css" /> 
     
     <title>Imobiliaria</title>
 </head>
 <body>
-    <h1> Imobiliaria Rocha <i class="glyphicon glyphicon-map-marker"></i></h1><br>
+    <h1> Imobiliaria Teste <i class="glyphicon glyphicon-map-marker"></i></h1><br>
   
     
     <div>
@@ -84,5 +84,18 @@ session_start();
             endforeach;
         endif;
     ?>
+
+<div id="footer">
+			<article id="copyright">&copy; copyright | Grupo Rocha - Engenharia | Todos os direitos reservados. 2015 | <a href="" id="mt">MT</a>.</article>
+        </div>
+        
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+            
 </body>
 </html>
